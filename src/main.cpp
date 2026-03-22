6eb53d76a8bcd3de49f96ee7c7ae114118a4674d
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2026-03-22 14:47:50 +0000

    feat:  integrate systems

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Utils/RunningOptions.hpp"
+#include "Api/Api.hpp"
 #include <print>
 
 int main(int argc, char** argv) {
@@ -8,13 +9,9 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    if (options.getCropwindow()) {
-        const auto& [x0, x1, y0, y1] = *options.getCropwindow();
-        std::println("Crop window: ({}, {}, {}, {})", x0, x1, y0, y1);
-    }
-    std::println("Quick render: {}", options.isQuick());
-    std::println("Output file: {}", options.getOutput());
-    std::println("Input scene file: {}", options.getInputSceneFile());
+    raytracer::Api::initEngine(options);
+    raytracer::Api::run();
+    raytracer::Api::cleanUp();
 
     return EXIT_SUCCESS;
 }
