d023f42f6dacee9ccc59330169ff34a516222d12
GabrielSilva66 <gabrieltedz.com@gmail.com>
2026-04-09 13:30:56 +0000

    feat: added methods for scene

--- include/Scene/Scene.cpp
+++ include/Scene/Scene.cpp
@@ -1,11 +1,39 @@
 #include "Scene.hpp"
 
-namespace raytracer {
+namespace raytracer {   
+   
    void Scene::addPrimitive(const std::shared_ptr<Primitive>& primitive) {
       _primitives.push_back(primitive);
    }
 
+   void Scene::addMaterial(const std::shared_ptr<Material>& material) {
+      _materialMap[material->getName()] = material;
+   }
+
+
    const std::vector<std::shared_ptr<Primitive>>& Scene::getPrimitives() const {
       return _primitives;
    }
+
+   Material* Scene::getMaterialAt(const std::string& name) const{
+      auto it = _materialMap.find(name);
+      if (it != _materialMap.end()) {
+         return it->second.get();
+      }
+      throw std::runtime_error("Material not found: " + name);
+   }
+
+   ParamSet Scene::getParam(const std::string& key) const {
+      auto it = _params.find(key);
+      if (it != _params.end()) {
+         return it->second;
+      }
+      throw std::runtime_error("Parameter not found: " + key);
+   }
+
+   void Scene::setParam(const std::string& key, const ParamSet& value) {
+      _params[key] = value;
+   }
+
+
 }
--- include/Scene/Scene.hpp
+++ include/Scene/Scene.hpp
@@ -4,19 +4,37 @@
 #include <vector>
 #include <memory>
 #include "Objects/Primitive.hpp"
+// #include "PrimitiveFactory.hpp"
+#include "Objects/Material.hpp"
+#include "Objects/MaterialFactory.hpp"
+#include <unordered_map>
+
+
 
 namespace raytracer {
+   using ParamSets = std::unordered_map<std::string, ParamSet>;
+
    class Scene {
       private:
          std::vector<std::shared_ptr<Primitive>> _primitives;
+         std::unordered_map<std::string, std::shared_ptr<Material>> _materialMap;
+         ParamSets _params;
          
       public:
          Scene() = default;
          ~Scene() = default;
 
          void addPrimitive(const std::shared_ptr<Primitive>& primitive);
+         void addMaterial(const std::shared_ptr<Material>& material);
          const std::vector<std::shared_ptr<Primitive>>& getPrimitives() const;
+
+         Material* getMaterialAt(const std::string& name) const;
+         void setParam(const std::string& key, const ParamSet& value);
+         ParamSet getParam(const std::string& key) const;
+         
+
    };
+   //set params e get params
 }
 
 #endif // !SCENE_HPP
