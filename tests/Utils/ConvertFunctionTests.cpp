31ec5afc38ae398a8a2135bba2db593ad6c3882c
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2026-05-14 14:28:54 +0000

    feat: add convert function to vector objets (IA)

--- tests/Utils/ConvertFunctionTests.cpp
+++ tests/Utils/ConvertFunctionTests.cpp
@@ -5,6 +5,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "Core/ParamSet.hpp"
+#include "Image/RGBColor.hpp"
 #include "Utils/ConvertFunction.hpp"
 
 TEST_CASE("convert<T> stores a valid integer value") {
@@ -93,6 +94,44 @@ TEST_CASE("convert<T,K,N> throws when more members than expected are provided") 
 
    REQUIRE_THROWS_AS(
       (convert<std::vector<int>, int, 3>("rgb", "10 20 30 40", &ps)),
+      std::invalid_argument
+   );
+}
+
+TEST_CASE("convert<std::vector<T>> stores a valid dynamic integer vector") {
+   raytracer::ParamSet ps;
+
+   REQUIRE(convert<std::vector<int>>("samples", "1 2 3 4 5", &ps));
+
+   const auto samples = ps.retrieve<std::vector<int>>("samples");
+   REQUIRE(samples.size() == 5);
+   REQUIRE(samples[0] == 1);
+   REQUIRE(samples[4] == 5);
+}
+
+TEST_CASE("convert<std::vector<RGBColor>> parses compact RGB triplets") {
+   raytracer::ParamSet ps;
+
+   REQUIRE(convert<std::vector<raytracer::RGBColor>>("palette", "255 255 255 0 0 0 255 0 0", &ps));
+
+   const auto palette = ps.retrieve<std::vector<raytracer::RGBColor>>("palette");
+   REQUIRE(palette.size() == 3);
+   REQUIRE(palette[0].getRed() == 255);
+   REQUIRE(palette[0].getGreen() == 255);
+   REQUIRE(palette[0].getBlue() == 255);
+   REQUIRE(palette[1].getRed() == 0);
+   REQUIRE(palette[1].getGreen() == 0);
+   REQUIRE(palette[1].getBlue() == 0);
+   REQUIRE(palette[2].getRed() == 255);
+   REQUIRE(palette[2].getGreen() == 0);
+   REQUIRE(palette[2].getBlue() == 0);
+}
+
+TEST_CASE("convert<std::vector<RGBColor>> throws when member count is not multiple of 3") {
+   raytracer::ParamSet ps;
+
+   REQUIRE_THROWS_AS(
+      convert<std::vector<raytracer::RGBColor>>("palette", "255 255 255 0", &ps),
       std::invalid_argument
    );
 }
